7a6e6fb0ddc547effbfce7108a01c7f63045fd43
Kevinmotanya <42073382+Kevinmotanya@users.noreply.github.com>
2019-02-21 18:21:50 -0600

    authentication

--- project-3/authentication/authentication.js
+++ project-3/authentication/authentication.js
@@ -13,4 +13,21 @@ var strategy = new Auth0Strategy (
     clientSecret: "VBCIB8L5V_qdoWzpy-2eB-l8nJFVJ9DenogKuUreHgZPVRQg8L3X8YqYUOCTYwju", 
     callbackURL: "http://localhost:3000/callback"
   },
-  
+  function (accessToken, refreshToken, extraParam, profile, done) {
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+passport.serializeUser(function(user, done){
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done){
+  done(null, user);
+});
+
+var app = express();
+
+app.set("views", path.join(__dirname, "
